0c4e2cd258e64b209b79a1f429b330286c9eb608
Marcus <marcusjian@awoo.com.tw>
2022-04-09 20:58:12 +0800

    create route and AppointmentController

---
+++ app/Http/Controllers/AppointmentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AppointmentController extends Controller
+{
+    public function book()
+    {
+        dd(__METHOD__);
+    }
+
+    public function cancel(int $id)
+    {
+        dd(__METHOD__);
+    }
+}
